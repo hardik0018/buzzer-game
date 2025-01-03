1fff531eec6b44e7eb1d11663abcabc2917bbc7c
Hardik Vatukiya <hardikvatukiya0014@gmail.com>
2025-01-03 07:59:44 +0530

    add toast funcationality

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -9,8 +9,8 @@ const Player = ({ playName, playNumber, score, isActive }) => {
         winner == playNumber ? "bg-green-400" : "bg-white"
       } shadow-md`}
     >
-      <h2 className="text-xl font-bold mb-4">Player :-{playName}</h2>
-      <h2 className="text-xl font-bold mb-4">Player :-{playNumber}</h2>
+      <h2 className="text-xl font-bold mb-4">Player Name:-{playName}</h2>
+      <h2 className="text-xl font-bold mb-4">Player Id :-{playNumber}</h2>
       {/* <div className="text-3xl font-bold mb-4">{score}</div> */}
     </div>
   );
